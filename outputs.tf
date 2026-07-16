dffd9a2a8dd8d88e8e56058d869e1183db515edb
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:45:52 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,17 @@
 output "api_management_api_releases_id" {
   description = "Map of id values across all api_management_api_releases, keyed the same as var.api_management_api_releases"
-  value       = { for k, v in azurerm_api_management_api_release.api_management_api_releases : k => v.id }
+  value       = { for k, v in azurerm_api_management_api_release.api_management_api_releases : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "api_management_api_releases_api_id" {
   description = "Map of api_id values across all api_management_api_releases, keyed the same as var.api_management_api_releases"
-  value       = { for k, v in azurerm_api_management_api_release.api_management_api_releases : k => v.api_id }
+  value       = { for k, v in azurerm_api_management_api_release.api_management_api_releases : k => v.api_id if v.api_id != null && length(v.api_id) > 0 }
 }
 output "api_management_api_releases_name" {
   description = "Map of name values across all api_management_api_releases, keyed the same as var.api_management_api_releases"
-  value       = { for k, v in azurerm_api_management_api_release.api_management_api_releases : k => v.name }
+  value       = { for k, v in azurerm_api_management_api_release.api_management_api_releases : k => v.name if v.name != null && length(v.name) > 0 }
 }
 output "api_management_api_releases_notes" {
   description = "Map of notes values across all api_management_api_releases, keyed the same as var.api_management_api_releases"
-  value       = { for k, v in azurerm_api_management_api_release.api_management_api_releases : k => v.notes }
+  value       = { for k, v in azurerm_api_management_api_release.api_management_api_releases : k => v.notes if v.notes != null && length(v.notes) > 0 }
 }
 
